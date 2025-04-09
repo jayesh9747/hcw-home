7411a0cb4710d506dffb9f5373940322b6a42191
jayesh@9747 <112215167@cse.iiitp.ac.in>
2025-04-09 15:20:23 +0530

    use best practises

--- backend/src/health/health.module.ts
+++ backend/src/health/health.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { HealthService } from './health.service';
 import { HealthController } from './health.controller';
-import { DatabaseModule } from '../database/database.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   imports: [DatabaseModule],
